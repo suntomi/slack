0cc7d6b72d5c5fd01791c740d214259bfde0cae6
Takehiro Iyatomi <iyatomi@gmail.com>
2025-08-01 11:21:58 +0900

    fix code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <command>"
-    exit 1
-fi
-COMMAND="$1"
-DATA="$2"
+TYPE="$1"
+COMMAND="$2"
+DATA="$3"
 if [ "${COMMAND}" = "match" ]; then
   if [ -z "$(echo "${DATA}" | jq -r '.slack')" ]; then
     echo ""
