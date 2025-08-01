6975bd53f249807847d70586c3574f5a1f654372
Takehiro Iyatomi <iyatomi@gmail.com>
2025-08-01 09:10:15 +0900

    fix for adjusting introduction of workflow module

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "nop"
+if [ -z "$1" ]; then
+    echo "Usage: $0 <command>"
+    exit 1
+fi
+COMMAND="$1"
+DATA="$2"
+if [ "${COMMAND}" = "match" ]; then
+  if [ -z "$(echo "${DATA}" | jq -r '.slack')" ]; then
+    echo ""
+  else
+    echo "${DATA}" | jq -r '.payload'
+  fi
+else 
+  echo ""
+fi
