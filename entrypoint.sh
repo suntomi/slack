a843e6f0be2dccda6a14f3cd7e60d8cfb7ace3b3
Takehiro Iyatomi <iyatomi@gmail.com>
2025-08-07 12:08:23 +0900

    don't return null when matching data structure does not exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ TYPE="$1"
 COMMAND="$2"
 DATA="$3"
 if [ "${COMMAND}" = "filter_event" ]; then
-  if [ -z "$(echo "${DATA}" | jq -r '.slack')" ]; then
+  if [ -z "$(printf '%s' "${DATA}" | jq -r '.discord // ""')" ]; then
     echo ""
   else
-    echo "${DATA}" | jq -r '.payload'
+    printf '%s' "${DATA}" | jq -r '.payload // ""'
   fi
 elif [ "${COMMAND}" = "filter_context" ]; then
   echo "match"
